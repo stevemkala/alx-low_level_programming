e8449fffb4cf1578108c41ff6b75dd34e213fc76
stevemkala <stevenmkala@gmail.com>
2023-03-19 13:06:23 +0000

    latest commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char d;
+	int a;
+	long int b;
+	long long int c;
+	float f;
+
+	printf("%d, %a, %b, %c, %f");
+	return (0);
+}
