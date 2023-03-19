fcec6257b9d9d67e657fc38ab0678d07583f2df4
stevemkala <stevenmkala@gmail.com>
2023-03-19 21:46:28 +0000

    latest commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print all single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	putchar('c');
+	putchar('\n');
+
+	return (0);
+}
