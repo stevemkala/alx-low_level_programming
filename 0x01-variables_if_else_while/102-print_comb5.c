04f40c98b15d04ddfcd1edae9e50ae97a01739cf
stevemkala <stevenmkala@gmail.com>
2023-03-20 20:59:55 +0000

    latest commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints all possible combinations
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int p, q;
+
+	for (p = '0'; p <= '98'; p++)
+	{
+
+	for (q = p + 1; q <= '99')
+	{
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+	putchar(' ');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0';)
+	if (p == 98 && p == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	putchar(' ');
+	return (0);
+}
