08b4af2b020147bb81f4940d2340c848955dc3d1
stevemkala <stevenmkala@gmail.com>
2023-03-20 21:03:46 +0000

    latest commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int p, q;
 
-	for (p = '0'; p <= '99'; p++)
+	for (p = '0'; p <= '98'; p++)
 	{
 
-	for (q = p + 1; q <= '99')
+	for (q = p + 1; q <= '99'; q++)
 	{
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
