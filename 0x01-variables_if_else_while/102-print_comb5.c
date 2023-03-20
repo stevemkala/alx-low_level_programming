da36f8aba5e51f23b84578b39e81ebf903104d9f
stevemkala <stevenmkala@gmail.com>
2023-03-20 21:31:13 +0000

    latest commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,23 +8,28 @@
  */
 int main(void)
 {
-	int p, q;
+	let num1 = 0, num2 = 1, num1max = 98, num2max = 99;
 
-	for (p = 0; p <= 98; p++)
+	while (num1 <= num1max)
 	{
+		num2 = num1 + 1;
+		while (num2 <= num2max)
+		{
+			putchar(num1 < 9 ? 0 + '0' : (num1 / 10) + '0');
+`			putchar(num1 < 9 ? num1 + '0' : (num1 % 10) + '0');
+			putchar(' ');
+			putchar(num2 < 9 ? 0 + '0' : (num2 / 10) + '0');
+			putchar(num2 < 9 ? num2 + '0' : (num2 % 10) + '0');
 
-	for (q = p + 1; q <= 99; q++)
-	{
-	putchar((p / 10) + '0');
-	putchar((p % 10) + '0');
-	putchar(' ');
-	putchar((q / 10) + '0');
-	putchar((q % 10) + '0');
-	if (p == 98 && p == 99)
-	continue;
-	putchar(',');
-	putchar(' ');
-	}
+			if (num1 != num1max)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+			num2++;
+		}
+		num1++;
 	}
 	putchar('\n');
 	return (0);
