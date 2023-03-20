02cdcd202338dd1618435abbcd3b5f65b9a7dc6c
stevemkala <stevenmkala@gmail.com>
2023-03-20 16:41:22 +0000

    latest commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print numbers of base 16
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char low;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n'')
+
+	return (0);
+}
