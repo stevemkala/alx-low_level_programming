5e7c9a405abc70d2d55a82f540434d4fde4703a1
stevemkala <stevenmkala@gmail.com>
2023-03-20 21:33:48 +0000

    latest commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 		while (num2 <= num2max)
 		{
 			putchar(num1 < 9 ? 0 + '0' : (num1 / 10) + '0');
-`			putchar(num1 < 9 ? num1 + '0' : (num1 % 10) + '0');
+			putchar(num1 < 9 ? num1 + '0' : (num1 % 10) + '0');
 			putchar(' ');
 			putchar(num2 < 9 ? 0 + '0' : (num2 / 10) + '0');
 			putchar(num2 < 9 ? num2 + '0' : (num2 % 10) + '0');
