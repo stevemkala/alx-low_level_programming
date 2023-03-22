b1b2abbec25d17744ff97290d0ad9d94507b9c02
stevemkala <stevenmkala@gmail.com>
2023-03-22 09:57:26 +0000

    latest commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - chacks for alphabetic character
+ * @c: charater to be checked
+ * Return: 1 if c is lowercase or uppercase otherwise 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
