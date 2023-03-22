d578acd05de54ec045079ed490b427185ee5ab7c
stevemkala <stevenmkala@gmail.com>
2023-03-22 11:17:47 +0000

    latest commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: par 1
+ * @b: par b
+ * Return: result of addition of the two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
