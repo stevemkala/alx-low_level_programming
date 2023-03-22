0092f454eb45ab09bb6413ed17d0522b399cb01a
stevemkala <stevenmkala@gmail.com>
2023-03-22 09:43:22 +0000

    latest commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - checks for lower case characters
+ * @c: character to be checked
+ * Return: 1 if character is lowercase
+ */
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z' c++)
+	{
+		return ('1');
+	}
+	else
+	{
+		return ('0');
+	}
+}
