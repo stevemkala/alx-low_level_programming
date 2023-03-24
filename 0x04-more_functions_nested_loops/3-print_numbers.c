3e5c0a9c25b263183a4ae4a884934e5c151caefd
stevemkala <stevenmkala@gmail.com>
2023-03-24 19:51:24 +0000

    latest commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from o to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+}
