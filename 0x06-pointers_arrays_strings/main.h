d0adbb67df3baf95ddfce068b4c57a1e061f1bd6
stevemkala <stevenmkala@gmail.com>
2023-03-31 20:11:57 +0000

    latest commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,13 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char c);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void rev_string(char *n);
+void print_buffer(char *b, int size);
 
 #endif
