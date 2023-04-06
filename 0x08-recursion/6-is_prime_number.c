5833d9ed2d033bc612a24613ba22156da974761b
stevemkala <stevenmkala@gmail.com>
2023-04-06 21:56:15 +0000

    latest commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+int actual_prime(int n, int i);
+/**
+ * is_prime_number - checks for prime number
+ * @n: int
+ * Return: 1 if prime number otherwise 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+/**
+ * actual_prime - checks for prime number
+ * @n: int
+ * @i: iterator
+ * Return: 1 if prime number otherwise 0
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
+}
