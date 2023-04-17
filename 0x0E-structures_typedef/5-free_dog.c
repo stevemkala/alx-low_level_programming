8a1e65094e76abecbf516064b1ae4e95fe07c3a3
stevemkala <stevenmkala@gmail.com>
2023-04-17 19:49:42 +0000

    latest commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees memory allocated to struct
+ * @d: struct to free
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
