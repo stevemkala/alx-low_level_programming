07b630e1f1dbec3a23100e112094bd9f500d4ae6
stevemkala <stevenmkala@gmail.com>
2023-04-20 22:30:10 +0000

    latest commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints name
+ * @name: string
+ * @f: pointer to function
+ * Return: Always 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
