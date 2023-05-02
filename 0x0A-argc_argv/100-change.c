78ed18fb6815524daa5495b51f6dd55d9b7d1948
stevemkala <stevenmkala@gmail.com>
2023-05-02 21:04:17 +0000

    latest commit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: array of input
+ * @argv: string of array
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num, j, result;
+
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+	num = atoi(argv[1]);
+
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
